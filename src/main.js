1de666ea144e3543e3ff1186015501372c9192c6
Linda <lindalobo.aguilar@gmail.com>
2023-04-01 16:29:26 -0300

    primera pagina

--- src/main.js
+++ src/main.js
@@ -1,6 +1,32 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+// import { example } from './data.js';
+// // import data from './data/lol/lol.js';
+// import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+let botonPokemon = document.getElementById("pokemon");
+
+let getData = () => {   fetch(
+    "./data/pokemon/pokemon.json"
+  )
+    .then((res) => res.json())
+    .then((res) => {
+      let pokemon = res.pokemon;
+      console.log(pokemon);
+      console.log(pokemon[0]);
+      const contenedor = document.getElementById("tarjeta2");
+      pokemon.forEach((pokemones) => {
+        let bloque = document.createElement("div");
+        bloque.innerHTML = ` <h2 class="nombrePoke"> ${pokemones.name}</h2> <img src="${pokemones.img}"> `;
+        contenedor.appendChild(bloque);
+      });
+    })
+    .catch((err) => console.error(err));
+
+}
+
+botonPokemon.addEventListener("click", () => {
+  document.getElementById("pokebola-img").setAttribute("style", "display:none");
+  document.getElementById("tarjeta1").setAttribute("style", "display:none").innerHTML = getData()
+
+
+});
