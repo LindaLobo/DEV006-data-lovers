aa9f5783a84072528f9d65aec35129c0283b4a10
angelica-rjs <angelicarojas.rys@gmail.com>
2023-04-03 09:42:08 -0400

    caga de iconos y fondos , div padre estilo y carga y magene

--- src/main.js
+++ src/main.js
@@ -10,15 +10,20 @@ let getData = () => {   fetch(
   )
     .then((res) => res.json())
     .then((res) => {
-      let pokemon = res.pokemon;
-      console.log(pokemon);
-      console.log(pokemon[0]);
+      let pokemones = res.pokemon;
+      console.log(pokemones);
+      console.log(pokemones[0]);
       const contenedor = document.getElementById("tarjeta2");
-      pokemon.forEach((pokemones) => {
+      pokemones.forEach((pokemon) => {
+        
         let bloque = document.createElement("div");
+        bloque.id = pokemones.name
         bloque.classList.add("bloquePokemones")
-        bloque.innerHTML = ` <h2 class="letrastarjetas"> ${pokemones.name}</h2> <img src="${pokemones.img}"> <h2 class="letrastarjetas">${pokemones.num} `;
+        bloque.classList.add(`pokemon-${pokemon.type[0]}`)
+        console.log(pokemon.type[0])
+        bloque.innerHTML = `<div class="bloquetransparente" id="tarjeta_${pokemon.name}"> <h2 class="letrastarjetas" > ${pokemon.type[0]}</h2> <img src="${pokemon.img}"> <h2 class="letrastarjetas">${pokemon.num} </div>`;
         contenedor.appendChild(bloque);
+        
       });
     })
     .catch((err) => console.error(err));
@@ -28,6 +33,7 @@ let getData = () => {   fetch(
 botonPokemon.addEventListener("click", () => {
   document.getElementById("pokebola-img").setAttribute("style", "display:none");
   document.getElementById("tarjeta1").setAttribute("style", "display:none").innerHTML = getData()
+
 
 
 });
