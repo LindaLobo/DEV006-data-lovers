d27f485c543e8331ff6a5a0d2df67fed461466dd
angelica-rjs <angelicarojas.rys@gmail.com>
2023-04-04 18:36:58 -0400

    cambios locales

--- src/main.js
+++ src/main.js
@@ -10,15 +10,15 @@ let getData = () => {
       // console.log(pokemones[0]);
       const contenedor = document.getElementById("tarjeta2");
       pokemones.forEach((pokemon) => {
-        // let raza = imageType(pokemon.type)
-        // console.log(raza)
+        let tipo1 = (pokemon.type)
+        let tipo2 = (pokemon.type)
         let bloque = document.createElement("div");
         bloque.id = pokemones.name;
         bloque.classList.add("bloquePokemones");
         bloque.classList.add(`pokemon-${pokemon.type[0]}`);
-        bloque.innerHTML = `<div class="bloquetransparente" id="tarjeta_"> <h2 class="letrastarjetas" > ${pokemon.name}
-        </h2> <img src="${pokemon.img}"> <div> <img id="boton-tipos" src=./img/iconos/${pokemon.type[0]}.png alt="tipo de pokemon"> 
-        </div> <h2 class="letrastarjetas">${pokemon.num} </div>`;
+        bloque.innerHTML = `<div class="bloquetransparente" id="tarjeta_"> <h2 class="letrastarjetas" > ${pokemon.name}</h2> <img src="${pokemon.img}">
+        <div> ${tipo1} </div>
+         <h2 class="letrastarjetas">${pokemon.num} </div>`;
         contenedor.appendChild(bloque);
       });
     })
