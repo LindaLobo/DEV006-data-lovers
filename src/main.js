931f16978820f5ffc45919ab80fc68d7aa5ae230
Linda <lindalobo.aguilar@gmail.com>
2023-04-04 16:49:34 -0400

    cambios del tipo pokemon

--- src/main.js
+++ src/main.js
@@ -1,37 +1,33 @@
-// import { example } from './data.js';
-// // import data from './data/lol/lol.js';
+// import { imageType } from './data.js';
 // import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-let botonPokemon = document.getElementById("pokemon");
-
-let getData = () => {   fetch(
-    "./data/pokemon/pokemon.json"
-  )
+let getData = () => {
+  fetch("./data/pokemon/pokemon.json")
     .then((res) => res.json())
     .then((res) => {
       let pokemones = res.pokemon;
-      console.log(pokemones);
-      console.log(pokemones[0]);
+      // console.log(pokemones);
+      // console.log(pokemones[0]);
       const contenedor = document.getElementById("tarjeta2");
       pokemones.forEach((pokemon) => {
-        
+        // let raza = imageType(pokemon.type)
+        // console.log(raza)
         let bloque = document.createElement("div");
-        bloque.id = pokemones.name
-        bloque.classList.add("bloquePokemones")
-        bloque.classList.add(`pokemon-${pokemon.type[0]}`)
-        console.log(pokemon.type[0])
-        bloque.innerHTML = `<div class="bloquetransparente" id="tarjeta_${pokemon.name}"> <h2 class="letrastarjetas" > ${pokemon.type[0]}</h2> <img src="${pokemon.img}"> <h2 class="letrastarjetas">${pokemon.num} </div>`;
+        bloque.id = pokemones.name;
+        bloque.classList.add("bloquePokemones");
+        bloque.classList.add(`pokemon-${pokemon.type[0]}`);
+        bloque.innerHTML = `<div class="bloquetransparente" id="tarjeta_"> <h2 class="letrastarjetas" > ${pokemon.name}
+        </h2> <img src="${pokemon.img}"> <div> <img id="boton-tipos" src=./img/iconos/${pokemon.type[0]}.png alt="tipo de pokemon"> 
+        </div> <h2 class="letrastarjetas">${pokemon.num} </div>`;
         contenedor.appendChild(bloque);
-        
       });
     })
     .catch((err) => console.error(err));
+};
 
-}
+let botonPokemon = document.getElementById("pokemon");
 
 botonPokemon.addEventListener("click", () => {
   document.getElementById("pokebola-img").setAttribute("style", "display:none");
-  document.getElementById("tarjeta1").setAttribute("style", "display:none").innerHTML = getData()
-
-});
+  document.getElementById("tarjeta1").setAttribute("style", "display:none").innerHTML = getData();
+})
