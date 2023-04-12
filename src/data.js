3a714d2c2f062d778b2ce4afee1b40e7b2cf1174
angelica-rjs <angelicarojas.rys@gmail.com>
2023-04-12 14:15:33 -0400

    eliminar img

--- src/data.js
+++ src/data.js
@@ -1,13 +1,18 @@
 // estas funciones son de ejemplo
 
-// export const imageType = (linda) => {
-//   let nuevo = ""
-//   pokemon.type.length > 1 ?
-//   pokemon.type.forEach(pokemon => {
-//     nuevo += `<p>${pokemon.type}</p>`: nuevo += `<p>${pokemon.type[0]}</p>`
-//   })
-// }
 
 export const anotherExample = () => {
   return "OMG";
 };
+
+
+[]pokemones
+
+let pokemonesfiltrados = pokemones.filer(
+  (pokemon) => {
+  
+    if(pokemon.type == "texto"){
+      return pokemon
+    }
+  }
+)
